3b5693c5b90b2899539f4f29d17d3c8cbcd401df
Bruno Benavent Colomo <brunobenaventcolomo@iMac-de-Bruno.local>
2025-04-08 15:55:46 +0200

    segundo commit

--- src/views/Products.tsx
+++ src/views/Products.tsx
@@ -1,9 +1,14 @@
-
+import { Link } from 'react-router-dom'
 
 export default function Products() {
   return (
-    <div>
-      Products
+    <div className='flex justify-between'>
+      <h2 className='text-4xl font-black text-slate-500'>Productos</h2>
+      <Link
+        to={'/productos/nuevo'}
+        className='rounded bg-indigo-600 p-3 text-sm font-bold text-white shadow-sm hover:bg-indigo-500'
+      
+      >Agregar Producto</Link>
     </div>
   )
 }
